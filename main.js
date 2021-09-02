ad035e3218f1c3a33d84caf7cc2499eb2154a258
sebasvisser <sebas.visser@mensenrelatie.nl>
2021-09-02 19:29:50 +0200

    documentation

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ async function findCountryData(event) {
         const name = "belgium";
         const result = await axios.get(`https://restcountries.eu/rest/v2/name/${name}`);
         console.log(result);
+
+        // OPDRACHT 2 Stadsinfo loggen
         const countryName = result.data[0].name;
         const population = result.data[0].population;
         const subAreaName = result.data[0].subregion;
         console.log(`${countryName} is situated in ${subAreaName}. It has a population of ${population} people.`)
+        // EINDE Stadsinfo loggen
 
         // OPDRACHT 3 hoofdstad loggen
         const city = result.data[0].capital;
